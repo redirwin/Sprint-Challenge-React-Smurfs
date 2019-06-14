10868a6c92945fc889cd1ca5ff756a85df995dda
Dave Irwin <david@redirwin.com>
2019-06-14 12:57:29 -0700

    Re-installed styled-components. Styled navbar.

--- village/src/components/Navigation.js
+++ village/src/components/Navigation.js
@@ -5,6 +5,16 @@ import styled from "styled-components";
 const NavWrapper = styled.div`
   with: 100%;
   padding: 2% 0;
+  background-color: #1e5799; // Old browsers
+  @include filter-gradient(#1e5799, #7db9e8, horizontal); // IE6-9
+  @include background-image(linear-gradient(left, #1e5799 0%, #7db9e8 36%));
+
+  a {
+    color: white;
+    text-decoration: none;
+    padding: 0 2%;
+    font-size: 1.25rem;
+  }
 `;
 
 class Navigation extends React.Component {
